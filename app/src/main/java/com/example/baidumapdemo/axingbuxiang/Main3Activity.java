1ffb91878af8985f30ce8719e4a3b8bb848af201
axingbuxiang <1083158034@qq.com>
2021-05-10 14:26:00 +0800

    增加的搜索框界面图

--- app/src/main/java/com/example/baidumapdemo/axingbuxiang/Main3Activity.java
+++ app/src/main/java/com/example/baidumapdemo/axingbuxiang/Main3Activity.java
@@ -3,12 +3,33 @@ package com.example.baidumapdemo.axingbuxiang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.baidumapdemo.R;
+
 
 public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main3);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String result = parent.getItemAtPosition(position).toString();
+                Toast.makeText(Main3Activity.this, result, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
+
