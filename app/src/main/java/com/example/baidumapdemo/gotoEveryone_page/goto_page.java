d9b2e08af4e29723141157f78524e966f3f338ba
Naz-mfg <1259528697@qq.com>
2021-05-10 10:51:37 +0800

    跳转wtz按钮

--- app/src/main/java/com/example/baidumapdemo/gotoEveryone_page/goto_page.java
+++ app/src/main/java/com/example/baidumapdemo/gotoEveryone_page/goto_page.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.example.baidumapdemo.R;
 import com.example.baidumapdemo.wangnaihao.Main.MainActivity;
+import com.example.baidumapdemo.wangtianzi.Main2Activity;
 
 public class goto_page extends AppCompatActivity {
 
@@ -18,6 +19,8 @@ public class goto_page extends AppCompatActivity {
         setContentView(R.layout.activity_goto_page);
         //注册王乃豪测试页面的按钮并设置点击事件
         init_wnh();
+        //注册王天滋测试页面的按钮并设置点击事件
+        init_wtz();
 
     }
     public void init_wnh(){
@@ -26,6 +29,17 @@ public class goto_page extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    public void init_wtz(){
+        Button wtz = findViewById(R.id.wtz);
+        wtz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
                 startActivity(intent);
                 finish();
             }
