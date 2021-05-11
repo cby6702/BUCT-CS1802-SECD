0f71c2d6eefe1221316101e92d864764eaa86c2d
gzy868 <2018040381@mail.buct.edu.cn>
2021-05-11 16:38:07 +0800

    gzy增加登录页面，个人中心，个人信息，我的评论，我的信息五个页面（这个页面之间跳转已经设置好）

--- app/src/main/java/com/example/baidumapdemo/guoziyu/mycomment.java
+++ app/src/main/java/com/example/baidumapdemo/guoziyu/mycomment.java
@@ -4,25 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.baidumapdemo.R;
 
-public class Main4Activity extends AppCompatActivity {
+public class mycomment extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main4);
-            init_gzy();
-        }
-    public void init_gzy(){
-        Button gzy = findViewById(R.id.button);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mycomment);
+        init_gzy1();
+    }
+    public void init_gzy1(){
+        Button gzy = findViewById(R.id.button10);
         gzy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,4 +29,3 @@ public class Main4Activity extends AppCompatActivity {
         });
     }
 }
-
