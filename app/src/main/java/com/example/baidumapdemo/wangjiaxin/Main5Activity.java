f4ee9bf16b7a134214f132f32225ceb717418379
Naz-mfg <1259528697@qq.com>
2021-05-13 16:40:12 +0800

    update 两个跳转页面

--- app/src/main/java/com/example/baidumapdemo/wangjiaxin/Main5Activity.java
+++ app/src/main/java/com/example/baidumapdemo/wangjiaxin/Main5Activity.java
@@ -2,11 +2,17 @@ package com.example.baidumapdemo.wangjiaxin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.baidumapdemo.R;
+import com.example.baidumapdemo.axingbuxiang.comment_museum;
+import com.example.baidumapdemo.wangnaihao.Main.MainActivity;
+import com.example.baidumapdemo.wangtianzi.Main2Activity;
 
 public class Main5Activity extends AppCompatActivity {
 
@@ -14,6 +20,41 @@ public class Main5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        Button button2=(Button)findViewById(R.id.button2);		//获取“进入地图页”按钮
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转地图页
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent2);
+                finish();
+            }
+        });
+
+        Button buttoneva=(Button)findViewById(R.id.eva);		//获取“前往评价”按钮
+        buttoneva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转评价页
+                Intent intent2 = new Intent(getApplicationContext(), comment_museum.class);
+                startActivity(intent2);
+                finish();
+            }
+        });
+
+        Button button1=(Button)findViewById(R.id.button);		//获取“上传视频”按钮
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转上传视频页
+               // Intent intent2 = new Intent(getApplicationContext(), .class);//设置跳转页面
+                //startActivity(intent2);
+                finish();
+            }
+        });
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
