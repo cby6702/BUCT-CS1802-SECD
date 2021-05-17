c61e60718d01e4c2799c656ef37f5a9448ba11df
zacoder9 <1536369788@qq.com>
2021-05-17 16:30:05 +0800

    邹傲 更新了首页ui 获取后台数据

---
+++ app/src/main/java/com/example/baidumapdemo/zouao/Bean/MuseumItem.java
@@ -0,0 +1,11 @@
+package com.example.baidumapdemo.zouao.Bean;
+
+public class MuseumItem {
+    public int itemImageResId;//图像资源ID
+    public String itemTitle;//标题
+
+    public MuseumItem(int itemImageResId, String itemTitle) {
+        this.itemImageResId = itemImageResId;
+        this.itemTitle = itemTitle;
+    }
+}
