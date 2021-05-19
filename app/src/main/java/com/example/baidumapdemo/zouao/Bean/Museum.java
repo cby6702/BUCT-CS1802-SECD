90a59b553b3d5f2b22dbd73a98a71c2a2446312d
zacoder9 <1536369788@qq.com>
2021-05-19 21:48:28 +0800

    邹傲 更新了首页布局 去掉了新闻列表项

--- app/src/main/java/com/example/baidumapdemo/zouao/Bean/Museum.java
+++ app/src/main/java/com/example/baidumapdemo/zouao/Bean/Museum.java
@@ -13,118 +13,108 @@ import com.example.baidumapdemo.R;
  */
 public class Museum {
 
-    private int mid;
-    private String name;
-    private String introduction;
-    private String opentime;
-    private double lat;
-    private String picture;
-    private String city;
-    private int exhibition_score;
-    private int environment_score;
-    private int service_score;
-    private int general_score;
-    private int annual_exhibitions;
-    private String ing;
-    private int itemImageResId= R.mipmap.ic_launcher;
-    public int getItemImageResId() {
-        return itemImageResId;
-    }
 
-    public void setItemImageResId(int itemImageResId) {
-        this.itemImageResId = itemImageResId;
-    }
+        private int mid;
+        private String name;
+        private String introduction;
+        private String opentime;
+        private double lat;
+        private String picture;
+        private String city;
+        private int exhibition_score;
+        private double environment_score;
+        private double service_score;
+        private double general_score;
+        private int annual_exhibitions;
+        private String ing;
+        public void setMid(int mid) {
+            this.mid = mid;
+        }
+        public int getMid() {
+            return mid;
+        }
 
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
 
-    public void setMid(int mid) {
-        this.mid = mid;
-    }
-    public int getMid() {
-        return mid;
-    }
+        public void setIntroduction(String introduction) {
+            this.introduction = introduction;
+        }
+        public String getIntroduction() {
+            return introduction;
+        }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
+        public void setOpentime(String opentime) {
+            this.opentime = opentime;
+        }
+        public String getOpentime() {
+            return opentime;
+        }
 
-    public void setIntroduction(String introduction) {
-        this.introduction = introduction;
-    }
-    public String getIntroduction() {
-        return introduction;
-    }
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+        public double getLat() {
+            return lat;
+        }
 
-    public void setOpentime(String opentime) {
-        this.opentime = opentime;
-    }
-    public String getOpentime() {
-        return opentime;
-    }
+        public void setPicture(String picture) {
+            this.picture = picture;
+        }
+        public String getPicture() {
+            return picture;
+        }
 
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-    public double getLat() {
-        return lat;
-    }
+        public void setCity(String city) {
+            this.city = city;
+        }
+        public String getCity() {
+            return city;
+        }
 
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-    public String getPicture() {
-        return picture;
-    }
+        public void setExhibition_score(int exhibition_score) {
+            this.exhibition_score = exhibition_score;
+        }
+        public int getExhibition_score() {
+            return exhibition_score;
+        }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getCity() {
-        return city;
-    }
+        public void setEnvironment_score(double  environment_score) {
+            this.environment_score = environment_score;
+        }
+        public double getEnvironment_score() {
+            return environment_score;
+        }
 
-    public void setExhibition_score(int exhibition_score) {
-        this.exhibition_score = exhibition_score;
-    }
-    public int getExhibition_score() {
-        return exhibition_score;
-    }
+        public void setService_score(double service_score) {
+            this.service_score = service_score;
+        }
+        public double getService_score() {
+            return service_score;
+        }
 
-    public void setEnvironment_score(int environment_score) {
-        this.environment_score = environment_score;
-    }
-    public int getEnvironment_score() {
-        return environment_score;
-    }
+        public void setGeneral_score(double general_score) {
+            this.general_score = general_score;
+        }
+        public double getGeneral_score() {
+            return general_score;
+        }
 
-    public void setService_score(int service_score) {
-        this.service_score = service_score;
-    }
-    public int getService_score() {
-        return service_score;
-    }
+        public void setAnnual_exhibitions(int annual_exhibitions) {
+            this.annual_exhibitions = annual_exhibitions;
+        }
+        public int getAnnual_exhibitions() {
+            return annual_exhibitions;
+        }
 
-    public void setGeneral_score(int general_score) {
-        this.general_score = general_score;
-    }
-    public int getGeneral_score() {
-        return general_score;
-    }
-
-    public void setAnnual_exhibitions(int annual_exhibitions) {
-        this.annual_exhibitions = annual_exhibitions;
-    }
-    public int getAnnual_exhibitions() {
-        return annual_exhibitions;
-    }
-
-    public void setIng(String ing) {
-        this.ing = ing;
-    }
-    public String getIng() {
-        return ing;
-    }
-
+        public void setIng(String ing) {
+            this.ing = ing;
+        }
+        public String getIng() {
+            return ing;
+        }
 }
--- app/src/main/java/com/example/baidumapdemo/zouao/Bean/MuseumRootBean.java
+++ app/src/main/java/com/example/baidumapdemo/zouao/Bean/MuseumRootBean.java
@@ -15,44 +15,54 @@ import java.util.List;
  */
 public class MuseumRootBean {
 
-    private int pageNo;
-    private int pageTotal;
-    private int pageSize;
-    private String pageTotalCount;
-    private List<Museum> items;
-    public void setPageNo(int pageNo) {
-        this.pageNo = pageNo;
-    }
-    public int getPageNo() {
-        return pageNo;
-    }
 
-    public void setPageTotal(int pageTotal) {
-        this.pageTotal = pageTotal;
-    }
-    public int getPageTotal() {
-        return pageTotal;
-    }
 
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-    public int getPageSize() {
-        return pageSize;
-    }
+    /**
+     * Auto-generated: 2021-05-19 21:39:27
+     *
+     * @author bejson.com (i@bejson.com)
+     * @website http://www.bejson.com/java2pojo/
+     */
 
-    public void setPageTotalCount(String pageTotalCount) {
-        this.pageTotalCount = pageTotalCount;
-    }
-    public String getPageTotalCount() {
-        return pageTotalCount;
-    }
 
-    public void setItems(List<Museum> items) {
-        this.items = items;
-    }
-    public List<Museum> getItems() {
-        return items;
-    }
+        private int pageNo;
+        private int pageTotal;
+        private int pageSize;
+        private String pageTotalCount;
+        private List<Museum> items;
+        public void setPageNo(int pageNo) {
+            this.pageNo = pageNo;
+        }
+        public int getPageNo() {
+            return pageNo;
+        }
+
+        public void setPageTotal(int pageTotal) {
+            this.pageTotal = pageTotal;
+        }
+        public int getPageTotal() {
+            return pageTotal;
+        }
+
+        public void setPageSize(int pageSize) {
+            this.pageSize = pageSize;
+        }
+        public int getPageSize() {
+            return pageSize;
+        }
+
+        public void setPageTotalCount(String pageTotalCount) {
+            this.pageTotalCount = pageTotalCount;
+        }
+        public String getPageTotalCount() {
+            return pageTotalCount;
+        }
+
+        public void setItems(List<Museum> items) {
+            this.items = items;
+        }
+        public List<Museum> getItems() {
+            return items;
+        }
 
 }
