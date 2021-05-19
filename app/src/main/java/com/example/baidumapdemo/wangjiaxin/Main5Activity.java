25d11f2fa29a0695d7c1f69f1e4b55a4db8f6554
Naz-mfg <1259528697@qq.com>
2021-05-19 23:27:50 +0800

    更新详情页 和其他页面的交互

--- app/src/main/java/com/example/baidumapdemo/wangjiaxin/Main5Activity.java
+++ app/src/main/java/com/example/baidumapdemo/wangjiaxin/Main5Activity.java
@@ -1,7 +1,6 @@
 package com.example.baidumapdemo.wangjiaxin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,24 +10,77 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.baidumapdemo.wangjiaxin.collectionexplain;
 import com.example.baidumapdemo.R;
-import com.example.baidumapdemo.wangjiaxin.HTTP.http_getmuseumbyname;
 import com.example.baidumapdemo.wangjiaxin.HTTP.http_getmuseummid;
-import com.example.baidumapdemo.wangtianzi.comment_museum;
+import com.example.baidumapdemo.wangjiaxin.museumexplain;
 import com.example.baidumapdemo.wangnaihao.Main.MainActivity;
+import com.example.baidumapdemo.wangtianzi.HttpGet_Zcomments;
+import com.example.baidumapdemo.wangtianzi.Usercomment;
+import com.example.baidumapdemo.wangtianzi.comment_museum;
+
+import java.util.List;
 
 public class Main5Activity extends AppCompatActivity {
 
     private String museumname;
     private int mid;
+    private RatingBar ratingBar;
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        Intent intentza = getIntent();
+        Bundle bundleza = intentza.getBundleExtra("Message");
+        if(bundleza!=null){
+            museumname = bundleza.getString("MuseumName");
+            TextView textView = findViewById(R.id.textView2);
+            textView.setText(museumname);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mid = http_getmuseummid.getText(museumname);
+                Message message=new Message();
+                message.what=1;
+                handler.sendMessage(message);
+            }
+        }).start();
 
+
+        handler=new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                int what = msg.what;
+                Log.i("handler", "已经收到消息，消息what：" + what + ",id:" + Thread.currentThread().getId());
+
+                if (what == 1) {                //进行列表加载
+                    Log.i("handler已接受到消息", "" + what);
+
+                    Log.e("test123",""+mid);
+                    Log.e("test456",museumname);
+                }
+            }
+        };
+        //这里是wtz加的，可以动态显示博物馆的星级
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);	//获取星级评分条
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.e("------------","当前的评价等级："+rating);
+                List<Usercomment> commentslist = HttpGet_Zcomments.getText(mid);//获取数据
+                System.out.println(commentslist);
+
+                int i= (int)commentslist.get(0).getGeneral_comment();//获取博物馆总评显示出来（用星标）
+                System.out.println(i);
+                ratingBar.setRating(i);
+            }
+        });
         Button button2=(Button)findViewById(R.id.button2);		//获取“进入地图页”按钮
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +99,10 @@ public class Main5Activity extends AppCompatActivity {
             public void onClick(View v) {
                 //跳转评价页
                 Intent intent2 = new Intent(getApplicationContext(), comment_museum.class);
-
-                Bundle bundle=new Bundle();
-               // bundle.putInt("number1",uid);//用户id
-                bundle.putInt("nummid",mid);//博物馆id
+                Bundle bundle = new Bundle();
+                bundle.putString("name",museumname);
+                bundle.putInt("nummid",mid);
+                Log.v("MIT",String.valueOf(mid));
                 intent2.putExtras(bundle);
                 startActivity(intent2);
                 finish();
@@ -62,18 +114,12 @@ public class Main5Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //跳转上传视频页
-               // Intent intent2 = new Intent(getApplicationContext(), .class);//设置跳转页面
+                // Intent intent2 = new Intent(getApplicationContext(), .class);//设置跳转页面
                 //startActivity(intent2);
                 //finish();
             }
         });
-        Intent intentza = getIntent();
-        Bundle bundleza = intentza.getBundleExtra("Message");
-        if(bundleza!=null){
-            String name = bundleza.getString("MuseumName");
-            TextView textView = findViewById(R.id.textView2);
-            textView.setText(name);
-        }
+
 
 
     }
@@ -86,12 +132,19 @@ public class Main5Activity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.museum_information:
-                Intent intentmuseum=new Intent(getApplicationContext(),museumexplain.class);
+                Intent intentmuseum=new Intent(getApplicationContext(), museumexplain.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("museumname",museumname);
+                intentmuseum.putExtras(bundle);
                 startActivity(intentmuseum);
                 break;
             case R.id.collection_information:
-                Intent intentcol=new Intent(getApplicationContext(),museumexplain.class);
+                Intent intentcol=new Intent(getApplicationContext(),collectionexplain.class);
+                Bundle bundlecol = new Bundle();
+                bundlecol.putString("colmid",String.valueOf(mid));
+                intentcol.putExtras(bundlecol);
                 startActivity(intentcol);
+                Toast.makeText(getApplicationContext(),mid+"",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.exhibition_information:
                 Intent intentex=new Intent(getApplicationContext(),museumexplain.class);
